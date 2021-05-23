9ed637509d17b822ca03cf4b8d79a1fc0bfcfc69
Dania-c <workingdania@hotmail.com>
2021-05-23 15:41:37 +0300

    Step7

---
+++ basic01/basic01_Step7.php
@@ -0,0 +1,12 @@
+<?php
+$birth_year=2005;
+$current_year=date("Y");
+
+if ( $current_year-$birth_year > 18)
+  {echo "You can drive ";}
+   else {
+     echo "You still a kid, go and play GTA";
+}
+
+
+?>
