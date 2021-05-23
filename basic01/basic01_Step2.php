ee36bb173a60b1df2a6dcc07272d14f350295717
Dania-c <workingdania@hotmail.com>
2021-05-23 09:02:49 +0300

    Step2

---
+++ basic01/basic01_Step2.php
@@ -0,0 +1,16 @@
+Hello world, Help me I am using PHP
+<?php
+
+?>
+Hello world, Help me I am using PHP
+</br>
+
+<?php
+echo "Search on how to enable PHP Errors";
+echo "<br>";
+
+print("I was getting errors but I can't see the errors");
+
+
+
+?>
