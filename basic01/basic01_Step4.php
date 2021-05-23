aad97f2c59879bdb4720655a635581e381ed45a6
Dania-c <workingdania@hotmail.com>
2021-05-23 09:04:36 +0300

    Step4

---
+++ basic01/basic01_Step4.php
@@ -0,0 +1,12 @@
+<?php
+$name = 'Gaby';
+$position = 'tech consultant ';
+$github_url ="https://github.com/gk-git";
+
+echo "method1 : Hello I'm  ".$name." I am a ".$position." check my github :".$github_url;
+echo "<br>";
+echo "method2 : Hello I'm ",$name," I am a ",$position," check my github :",$github_url;
+echo "<br>";
+echo "method3 : Hello I'm $name I am a $position check my github : $github_url ";
+
+?>
