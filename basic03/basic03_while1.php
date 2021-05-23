25fd153ffc82c1abb6b2394fbbc479c08ee31863
Dania-c <workingdania@hotmail.com>
2021-05-24 00:44:22 +0300

    _while1

---
+++ basic03/basic03_while1.php
@@ -0,0 +1,15 @@
+<?php
+$x1=1;
+while ($x1<=8)
+{
+//echo "Increment Number : $x1 \n";
+    $j=1;
+    while ($j<=$x1)
+    {
+        echo "*";
+        $j=$j+1;
+    }
+    echo "<br>";
+    $x1=$x1+1;
+}
+?>
