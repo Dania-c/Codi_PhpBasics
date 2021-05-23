0262ac917eaf02dff2928019b50f1c5a9f474359
Dania-c <workingdania@hotmail.com>
2021-05-23 17:01:07 +0300

    step8-a

---
+++ basic01/basic01_Step8-a.php
@@ -0,0 +1,8 @@
+<?php
+$grocery = array("Eggs", "Milk", "Cheese","Water Pack","Tissues","Watermelon");
+$lastitem =count($grocery) -1;
+echo " Hello Sir, do you have $grocery[0], $grocery[1], and $grocery[2]? ";
+echo "<br>";
+echo "Also if you sell fruits ,can I find a $grocery[$lastitem]?";
+
+?>
