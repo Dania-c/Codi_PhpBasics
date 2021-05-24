a35ce4c50865182122098dd15f9f1fdc708cd612
Dania-c <workingdania@hotmail.com>
2021-05-24 21:39:50 +0300

    for 4

--- basic03/basic03_for2.php
+++ basic03/basic03_for2.php
@@ -2,7 +2,7 @@
 $e1 = 3; 
 $e2 = 4;
 $di = 5;
-
+function checkQuadri($e1,$e2,$di){
 $rec = 0;
 $dia = 0;   
     if ($e1 * $e1 + $e2 * $e2 === $di * $di) {
@@ -15,4 +15,6 @@ if ($rec>0)
 echo "This is a rectangle.";
 if ($dia>0)
 echo "This is a rhombus.";
+}
+checkQuadri(3,4,5);
 ?>
--- basic03/basic03_for4.php
+++ basic03/basic03_for4.php
@@ -1,12 +1,15 @@
 <?php
-for($i=1;$i<=7;$i++){
-    for($j=1;$j<=7;$j++){
-        if(($i==1)||($i==7) || ($i==$j)){
-            echo "*";
-        }else{
-            echo "&nbsp;&nbsp;";
+function drawZ (){
+    for($i=1;$i<=7;$i++){
+        for($j=1;$j<=7;$j++){
+            if(($i==1)||($i==7) || ($i==$j)){
+                echo "*";
+            }else{
+                echo "&nbsp;&nbsp;";
+            }
         }
+        echo "<br>";
     }
-    echo "<br>";
 }
+drawZ();
 ?>
